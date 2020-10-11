f800f9a01c1848098ce29551ea21d5c1e3358932
Archit Gupta <archit.gupta@booking.com>
2020-10-11 20:43:11 +0530

    Init a blank SpringBoot + Maven project.

---
+++ src/main/java/com/archit/biddingframework/BiddingFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.archit.biddingframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archit/biddingframework/BiddingFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archit.biddingframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
